cadf0a4b906c22d7dcc5bb687dc65004ed657f65
Calebe-Dev <calebe.araujo101@gmail.com>
2025-01-26 13:06:37 -0300

    Polimento navbar, hero e Social banner

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,10 +5,11 @@ import { SocialBannerComponent } from "../social-banner/social-banner.component"
 import { ProductsBaannerComponent } from "../products-baanner/products-baanner.component";
 import { FeedbacksComponent } from "../feedbacks/feedbacks.component";
 import { FooterComponent } from "../footer/footer.component";
+import { AboutMeComponent } from "../about-me/about-me.component";
 
 @Component({
   selector: 'app-home',
-  imports: [NavbarComponent, HeroComponent, SocialBannerComponent, ProductsBaannerComponent, FeedbacksComponent, FooterComponent],
+  imports: [NavbarComponent, HeroComponent, SocialBannerComponent, ProductsBaannerComponent, FeedbacksComponent, FooterComponent, AboutMeComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
